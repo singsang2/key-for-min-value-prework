d2b450596af390688576629095349a1e217b4e05
Sung Bae <singsang2@gmail.com>
2018-10-08 01:35:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,11 @@
 
 def key_for_min_value(name_hash)
   thing = nil
-  val = name_hash[0]
-  if name_hash.size != 0
-    name_hash.each do |name, number|
-      if val == number || val < number
-        thing = name
-        val = number
-      end
+  val = name_hash.first[1]
+  name_hash.each do |name, number|
+    if val == number || val < number
+      thing = name
+      val = number
     end
   end
   
